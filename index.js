f1b859a5080563e4932833edcc2130e7f506da37
Rachel <rgabelarde@gmail.com>
2023-09-23 17:45:32 +0800

    Update index.js to add routes for user, chat, messages and suspiciousActivityLogs

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const mongoString = process.env.DATABASE_URL;
-const PORT = process.env.PORT ?? 3000;
-const routes = require("./routes/routes");
 
-mongoose.connect(mongoString);
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const suspiciousActivityLogRoutes = require("./routes/suspiciousActivityLogRoutes");
+
+const mongoURL = process.env.DATABASE_URL;
+const PORT = process.env.PORT ?? 3000;
+
+mongoose.connect(mongoURL);
 const database = mongoose.connection;
 
 database.on("error", (error) => {
@@ -19,7 +24,11 @@ database.once("connected", () => {
 
 const app = express();
 app.use(express.json());
-app.use("/api", routes);
+
+app.use("/api/user", userRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/report", suspiciousActivityLogRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
