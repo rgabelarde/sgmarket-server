abac51ef9d720bd076f0bc10724a608a5aba061d
Rachel <rgabelarde@gmail.com>
2023-09-24 19:26:17 +0800

    Add workflows to deploy using hooks

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
 var express = require("express");
 var mongoose = require("mongoose");
-var path = require("path");
 var cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
@@ -12,26 +13,51 @@ const listingRoutes = require("./routes/listingRoutes");
 const reservationRoutes = require("./routes/reservationRoutes");
 const suspiciousActivityLogRoutes = require("./routes/suspiciousActivityLogRoutes");
 
-const mongoURL = process.env.DATABASE_URL || process.env.MONGO_URI;
 const PORT = process.env.PORT ?? 8080;
-
-mongoose.connect(mongoURL);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
-});
-
-database.once("connected", () => {
-  console.log("Database Connected");
-});
-
 const app = express();
+
+// config cookie-parser
+app.use(cookieParser());
+
+// config cors so that front-end can use
+app.use(cors());
+app.options((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, PATCH, POST, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+}, cors());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// Configure bodyparser to handle post requests
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+// Connect to Mongoose and set connection variable
+let mongoDB =
+  process.env.ENV == "DEV"
+    ? process.env.DB_LOCAL_URI
+    : process.env.DATABASE_URL;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Added check for DB connection
+if (!db) console.log("Error connecting db");
+else console.log("Db connected successfully");
 
 app.use("/api/user", userRoutes);
 app.use("/api/chats", chatRoutes);
